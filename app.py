30c14a60f88ca40593a568f629afcb816758c165
Yashank Chopra <111984301+yashankchopra@users.noreply.github.com>
2022-10-11 21:51:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,8 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
-
+import json
+from flask import Flask
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-
 @app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
-
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+def index():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+app.run()
